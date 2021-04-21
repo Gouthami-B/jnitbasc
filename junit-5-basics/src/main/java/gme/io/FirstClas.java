dc246acbc516d83223af9357d6496aac064af339
Gouthami-B <74284773+Gouthami-B@users.noreply.github.com>
2021-04-21 20:36:08 +0530

    made changes to develop

--- junit-5-basics/src/main/java/gme/io/FirstClas.java
+++ junit-5-basics/src/main/java/gme/io/FirstClas.java
@@ -5,8 +5,7 @@ public class FirstClas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hi am in branch master");
-//added a new line here
-		//making some changes
+
 	}
 
 }
